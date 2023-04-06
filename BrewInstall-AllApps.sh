b61afd63605b14468b76f7468f4f8c121aa85620
Emanuele Ciurleo <emanuele@ciurleo.com>
2023-04-06 15:12:41 +0100

    first commit
    Using Brew installs the most common apps

---
+++ BrewInstall-AllApps.sh
@@ -0,0 +1,60 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+##############################
+### For All Users
+##############################
+
+#Install Micorsoft office
+brew install --cask microsoft-office 
+#Install Airwatch Client
+brew install --cask workspace-one-intelligent-hub
+#Install Chrome
+brew install --cask google-chrome
+#Install Slack
+brew install --cask slack
+#Install Teams
+brew install --cask microsoft-teams
+#Install AWS VPN Client
+brew install --cask aws-vpn-client
+#Install Lastpass Desktop App
+brew install  --cask lastpass
+#Install Adobe Acrobat Reader
+brew install --cask adobe-acrobat-reader
+
+##############################
+### For Devs
+##############################
+#Install Visual Studio Code
+brew install --cask visual-studio-code
+#Install IntelliJ
+brew install --cask intellij-idea-ce
+#Install iTerm2
+brew install --cask iTerm2
+#Install Docker Desktop
+brew install  --cask docker
+#Install VirtualBox
+brew install --cask virtualbox
+#Install Postman
+brew install --cask postman
+#Install AWS Cli
+brew install awscli
+#Install Box Drive
+brew install --cask box-drive
+
+##############################
+### Extras
+##############################
+#Install Rectangle (window snap manager)
+brew install --cask rectangle
+#Install Adobe Creative Cloud (Paid)
+brew install --cask adobe-creative-cloud
+#Install OWASP Zap! 
+brew install --cask owasp-zap
+#Install IntelliJ (Paid)
+brew install --cask intellij-idea
+#Github Desktop
+brew install --cask github
+#Install Firefox
+brew install --cask firefox 
+#Install Microsoft Edge
+brew install --cask microsoft-edge
